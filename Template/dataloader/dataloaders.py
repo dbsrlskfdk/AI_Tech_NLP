0a94719e0732f031b7e05da4e0cb11190c122ffa
name_lig96 <126560547+lig96@users.noreply.github.com>
2023-04-27 16:58:57 +0900

    Module: Detach make_sampler function

--- Template/dataloader/dataloaders.py
+++ Template/dataloader/dataloaders.py
@@ -106,23 +106,7 @@ class Dataloader(pl.LightningDataModule):
             self.predict_dataset = Dataset(predict_inputs, [])
 
     def train_dataloader(self):
-        def make_sampler(train_data):
-            train_data['class'] = train_data['label'].apply(
-                lambda x: int(x//1) if x != 5 else int(4))
-            class_counts = train_data['class'].value_counts().to_list()
-            # [3711, 1715, 1393, 1368, 1137]
-            labels = train_data['class'].to_list()
-            # [2, 4, 2, 3, 0, 2, 3, 0,
-            num_samples = sum(class_counts)
-            class_weights = [num_samples/class_counts[i]
-                             for i in range(len(class_counts))]
-            # [2.51, 5.436, 6.69, 6.81, 8.20]
-            weights = [class_weights[labels[i]]
-                       for i in range(int(num_samples))]
-            # [6.69, 8.20, 6.69, 6.81, 2.51, 6.69
-            sampler = torch.utils.data.WeightedRandomSampler(
-                torch.DoubleTensor(weights), int(num_samples))
-            return sampler
+        # def make_sampler(train_data):
 
         # shuffle=args.shuffle
         return torch.utils.data.DataLoader(self.train_dataset, batch_size=self.batch_size, num_workers=8, sampler=make_sampler(self.after_aug_train_data))
